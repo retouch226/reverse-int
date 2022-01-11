bff0c21e5a8170a674ff2516e3fc173d922e8ee5
Ivan <retouch226@gmail.com>
2022-01-11 20:36:09 +0200

    init: 20 passing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let numberString = String(n).split("");
+    if (numberString[0] === "-") {
+        numberString.splice(0, 1);
+    }
+    let temp = "";
+    for (let i = 0; i < numberString.length - 2; i++) {
+        temp = numberString[i];
+        numberString[i] = numberString[numberString.length - 1 - i];
+        numberString[numberString.length - 1 - i] = temp;
+    }
+    numberString = numberString.join("");
+    return Math.abs(+numberString);
+};
+//console.log(reverse(-425));
